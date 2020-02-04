55ea0f203d012261d8a90d674e6435956e789981
Randall Phillips <rphill45@uncc.edu>
2020-02-04 13:03:35 -0500

    setup passport auth and routed for Login, register, upcoming features and a Could not find page

--- server/src/index.js
+++ server/src/index.js
@@ -5,32 +5,42 @@ const { setupDB } = require("../src/config/databaseConnection");
 const cors = require("cors");
 const path = require("path");
 
-var PORT = process.env.PORT || 4000;
+const { passport } = require("./lib/passport");
 
-// This is a comment
+var PORT = process.env.PORT || 4000;
 
 const app = express();
 setupDB(v => console.log(v));
 
 app.use(cors());
 app.use(
-    '/graphql',
-    graphqlHTTP({
-        schema,
-        graphql: true,
-        pretty: true
-    })
-    );
-    
-    if (process.env.NODE_ENV === 'production') {
-        const clientBuildPath = path.join(__dirname, '..','..', 'jury-box-frontend', 'build');
-        console.log(`Client build path: ${clientBuildPath}\n`);
-        app.use(express.static(clientBuildPath));
-      }
-      app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '..','..','jury-box-frontend','build','index.html'));
-      });
-      
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphql: true,
+    pretty: true
+  })
+);
+app.use(passport.initialize());
 
-    app.listen(PORT);
+if (process.env.NODE_ENV === "production") {
+  const clientBuildPath = path.join(
+    __dirname,
+    "..",
+    "..",
+    "jury-box-frontend",
+    "build"
+  );
+  console.log(`Client build path: ${clientBuildPath}\n`);
+  app.use(express.static(clientBuildPath));
+}
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "..", "..", "jury-box-frontend", "build", "index.html")
+  );
+});
+
+app.use(require("./controllers"));
+
+app.listen(PORT);
 console.log("SERVER OKAY!");
